eb5b6f3f317f54b0ce11715630d5a71e3e9ab71e
mortalcr <daporrasn@gmail.com>
2024-09-15 20:51:20 -0600

    refactor: remove menu button

--- src/views/TabsPage.vue
+++ src/views/TabsPage.vue
@@ -1,26 +1,6 @@
 <template>
   <ion-page>
     <ion-tabs>
-      <ion-menu contentId="main-content">
-        <ion-header>
-          <ion-toolbar color="gray">
-            <ion-title>Navegar</ion-title>
-          </ion-toolbar>
-        </ion-header>
-        <ion-content class="ion-padding">
-          hola
-        </ion-content>
-      </ion-menu>
-      <div class="ion-page" id="main-content">
-        <ion-header>
-          <ion-toolbar>
-            <ion-buttons slot="start">
-              <ion-menu-button></ion-menu-button>
-            </ion-buttons>
-            <ion-title>Rick and Morty App</ion-title>
-          </ion-toolbar>
-        </ion-header>
-      </div> 
       <ion-router-outlet></ion-router-outlet>
       <ion-tab-bar slot="bottom">
         <ion-tab-button tab="tab1" href="/tabs/tab1">
