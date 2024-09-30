7b086948b1f3bbf60cb8a5c5e5ec30547a82640f
noniLaptop <noni45100@hotmail.com>
2024-09-30 11:22:06 -0600

    feat: nodos

--- src/views/Tab3Page.vue
+++ src/views/Tab3Page.vue
@@ -14,6 +14,7 @@
       <ion-searchbar color="light" v-model="searchQuery" :debounce="500" @ionInput="filterEpisodes"
         placeholder="Buscar episodios..."></ion-searchbar>
       <ion-list>
+        <div class="graph-container" id="legraph"></div>
         <ion-item v-for="episode in episodes" :key="episode.id" button :href="`/episode/${episode.id}`">
           <ion-avatar slot="start">
           </ion-avatar>
@@ -34,6 +35,11 @@ import { ref, onMounted } from 'vue';
 import axios from 'axios';
 import { IonPage, IonHeader, IonToolbar, IonTitle, IonContent, IonSearchbar, IonList, IonItem, IonAvatar, IonLabel } from '@ionic/vue';
 
+import { Result, Root } from '@/characters-root';
+import { ActionSheetButton } from '@ionic/vue';
+import { Network, Data, Edge, Node, Options, DataSet } from "vis-network/standalone";
+import { add } from 'ionicons/icons';
+
 interface Episode {
   id: number;
   name: string;
@@ -42,29 +48,44 @@ interface Episode {
   characters: string[];
 }
 
+interface Character {
+  id: number;
+  image: string;
+  name: string;
+}
+
 const episodes = ref<Episode[]>([]);
 const nextPage = ref<string | null>('https://rickandmortyapi.com/api/episode');
-const searchQuery = ref('');  
+const searchQuery = ref('');
 
+const nodes = new DataSet<Node>([]);
+const edges = new DataSet<Edge>([]);
 
 const loadEpisodes = async () => {
   if (nextPage.value) {
     const response = await axios.get(nextPage.value);
     const results: Episode[] = response.data.results;
-    
+
     results.forEach((episode) => {
       if (episode.name.toLowerCase().includes(searchQuery.value.toLowerCase())) {
         episodes.value.push(episode);
       }
     });
-    
+
     if (response.data.info.next) {
       nextPage.value = response.data.info.next;
       loadEpisodes();
     }
-    
+
   }
 };
+
+const loadCharacter = async (characterUrl: string) => {
+
+  const result: Character = await axios.get(characterUrl);
+
+  return { result };
+}
 
 const filterEpisodes = () => {
   episodes.value = [];
@@ -72,10 +93,42 @@ const filterEpisodes = () => {
   loadEpisodes();
 };
 
+async function prepareGraph() {
+  for (const element of episodes.value) {
+    for (const characterUrl of element.characters) {
+      const character = (await loadCharacter(characterUrl)).result;
+      nodes.update({
+        id: `character-${element.id}`,
+        image: character.image,
+        shape: "circularImage",
+        label: character.name,
+      });
+      nodes.update({
+        id: element.id,
+        color: '#cccccc',
+        shape: "square",
+        label: element.name,
+      });
+      edges.update({
+        to: `character-${character.id}`,
+        from: element.id,
+      } as Edge);
+    }
+  }
+}
 
-
-onMounted(() => {
-  loadEpisodes();
+onMounted(async () => {
+  await loadEpisodes();
+  await prepareGraph();
+  const container = document.getElementById("legraph") as HTMLElement;
+  const data: Data = {
+    nodes: nodes,
+    edges: edges,
+  };
+  const options: Options = {
+    autoResize: false,
+  };
+  new Network(container, data, options);
 });
 
 
@@ -107,5 +160,10 @@ ion-select {
   --background: #fffeef;
   --color: #000000;
 }
-</style>
 
+.graph-container {
+  width: 100%;
+  height: 100%;
+}
+
+</style>
