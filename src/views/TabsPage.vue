fc0f8ed28d68811ad15db0477805b03634576afa
zSnails <erizojuan33@gmail.com>
2024-10-17 21:10:49 -0600

    feat: add account tab button

--- src/views/TabsPage.vue
+++ src/views/TabsPage.vue
@@ -1,10 +1,24 @@
 <template>
-  <ion-page>
+  <!-- <ion-menu content-id="main-content">
+    <ion-header>
+      <ion-toolbar>
+        <ion-title>Leiner gay</ion-title>
+      </ion-toolbar>
+    </ion-header>
+    <ion-content>
+      <ion-list>
+        <ion-item>
+          <ion-button slot="start">Cerrar Sesión</ion-button>
+        </ion-item>
+      </ion-list>
+    </ion-content>
+  </ion-menu> -->
+  <ion-page id="main-content">
     <ion-tabs>
       <ion-router-outlet></ion-router-outlet>
       <ion-tab-bar slot="bottom">
         <ion-tab-button tab="tab1" href="/tabs/tab1">
-          <ion-icon aria-hidden="true" :icon="person" />
+          <ion-icon aria-hidden="true" :icon="people" />
           <ion-label>Personajes</ion-label>
         </ion-tab-button>
 
@@ -21,6 +35,11 @@
           <ion-icon aria-hidden="true" :icon="planet" />
           <ion-label>Ubicaciones</ion-label>
         </ion-tab-button>
+
+        <ion-tab-button tab="tab5" href="/tabs/account">
+          <ion-icon aria-hidden="true" :icon="person" />
+          <ion-label>Cuenta</ion-label>
+        </ion-tab-button>
       </ion-tab-bar>
     </ion-tabs>
   </ion-page>
@@ -28,5 +47,5 @@
 
 <script setup lang="ts">
 import { IonTabBar, IonTabButton, IonTabs, IonLabel, IonIcon, IonPage, IonRouterOutlet } from '@ionic/vue';
-import { tv, planet, home, person } from 'ionicons/icons';
+import { tv, planet, home, person, people } from 'ionicons/icons';
 </script>
